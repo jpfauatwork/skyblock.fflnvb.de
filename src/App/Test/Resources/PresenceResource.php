ddc7bf84b8d7e69987a19d969a79c7356c709c6a
Jan Pfau <138393440+jpfauatwork@users.noreply.github.com>
2024-07-22 23:00:28 +0200

    fix: adding missing resource

---
+++ src/App/Test/Resources/PresenceResource.php
@@ -0,0 +1,28 @@
+<?php
+ 
+namespace App\Test\Resources;
+ 
+use Domain\Presence\Models\Presence;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+ 
+/**
+ * @mixin Presence
+ */
+class PresenceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->player->name,
+            'joined_at' => $this->joined_at,
+            'left_at' => $this->left_at,
+        ];
+    }
+}
