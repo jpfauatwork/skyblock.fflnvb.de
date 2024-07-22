38410f3d44a2c8ec66c351e3ce9140e277192c09
Jan Pfau <138393440+jpfauatwork@users.noreply.github.com>
2024-07-22 22:59:11 +0200

    feature: beta route for insights

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Test\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/presences/recent', TestController::class);
---
+++ src/App/Test/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Test;
+
+use App\Test\Resources\PresenceResource;
+use Domain\Presence\Models\Presence;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TestController
+{
+    public function __invoke(): ResourceCollection
+    {
+        $presences = Presence::query()
+            ->with('player')
+            ->orderBy('joined_at', 'desc')
+            ->limit(100)
+            ->get();
+
+        return PresenceResource::collection($presences);
+    }
+}
