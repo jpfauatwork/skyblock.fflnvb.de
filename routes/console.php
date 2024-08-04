2ba1d75b70af6404a3f29970bc7a7efa596be1b5
Jan Pfau <138393440+jpfauatwork@users.noreply.github.com>
2024-08-04 22:12:23 +0200

    adding param to scheduler command

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use App\Console\Commands\ScanSkyblockServerCommand;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(ScanSkyblockServerCommand::class)->everyMinute();
+Schedule::command(ScanSkyblockServerCommand::class, ['server' => 'economy'])->everyMinute();
