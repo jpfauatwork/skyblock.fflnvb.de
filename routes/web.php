e9354376f5a2e33ed5fc0628c8502322103589ed
Jan Pfau (Smake IT) <githubworkg9@fflnvb.de>
2025-06-17 23:56:24 +0200

    improve routing

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
-use App\PublicApi\Playground\Controllers\PlaygroundController;
 use App\PublicApi\Presences\Controllers\ListPresencesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/public-api/presences');
 
 Route::prefix('public-api')
     ->name('public-api.')
     ->middleware([])
     ->group(function () {
-        Route::get('/playground', PlaygroundController::class);
         Route::prefix('presences')
             ->name('presences.')
             ->group(function () {
